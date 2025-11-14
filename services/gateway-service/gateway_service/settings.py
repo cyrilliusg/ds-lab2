0d08dd4a9e17be259cf286aa020bce7444f61d33
cyrilliusg <kirillgaynov@gmail.com>
2025-11-14 12:07:37 +0300

    services requests approach refactor and implementing logging

--- services/gateway-service/gateway_service/settings.py
+++ services/gateway-service/gateway_service/settings.py
@@ -47,6 +47,40 @@ DATABASES = {
 
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(levelname)s %(name)s: %(message)s",
+        },
+    },
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        }
+    },
+
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+
+        # Логи нашего модуля с клиентами (пример: app/services/client.py)
+        "gateway_service.gateway": {  # ← поменяй на свой путь!
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
 # --------------------------------------------------------------------------
 
 
